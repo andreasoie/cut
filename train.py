e36dd86754a7d7af40b649a7d95e1fc1df6a3510
andreasoie <oeie.andreas@gmail.com>
2023-01-25 15:36:17 +0100

    Added repofiles

---
+++ train.py
@@ -0,0 +1,107 @@
+import os
+import time
+import warnings
+from collections import OrderedDict
+
+import torch
+from matplotlib import pyplot as plt
+
+import wandb
+from data import create_dataset
+from models import create_model
+from options.train_options import TrainOptions
+from util.util import tensor2im
+
+warnings.filterwarnings("ignore")
+
+os.makedirs("tmp", exist_ok=True)
+os.makedirs("checkpoints", exist_ok=True)
+
+def save_snapshot_image(visuals: OrderedDict) -> None:
+    fig, axs = plt.subplots(nrows=1, ncols=len(visuals), squeeze=False, figsize=(40, 10))
+    for i, (label, image) in enumerate(visuals.items()):
+        image = tensor2im(image)
+        axs[0, i].imshow(image)
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+        axs[0, i].set_title(label, fontsize=25)
+    plt.savefig(f"tmp/current.png")
+    plt.tight_layout()
+    plt.close()
+
+if __name__ == '__main__':
+
+
+    opt = TrainOptions().parse()   # get training options
+    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    dataset_size = len(dataset)    # get the number of images in the dataset.
+
+    model = create_model(opt)      # create a model given opt.model and other options
+    print('The number of training images = %d' % dataset_size)
+
+    total_iters = 0                # the total number of training iterations
+    optimize_time = 0.1
+
+    wandb.init(project="cut", entity="andreasoie")
+    wandb.config.update(opt)
+    
+    times = []
+    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+        epoch_start_time = time.time()  # timer for entire epoch
+        iter_data_time = time.time()    # timer for data loading per iteration
+        epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
+
+        dataset.set_epoch(epoch)
+
+        for i, data in enumerate(dataset):  # inner loop within one epoch
+            iter_start_time = time.time()  # timer for computation per iteration
+            if total_iters % opt.print_freq == 0:
+                t_data = iter_start_time - iter_data_time
+
+            batch_size = data["A"].size(0)
+            total_iters += batch_size
+            epoch_iter += batch_size
+
+            if len(opt.gpu_ids) > 0:
+                torch.cuda.synchronize()
+
+            optimize_start_time = time.time()
+
+            if epoch == opt.epoch_count and i == 0:
+                model.data_dependent_initialize(data)
+                model.setup(opt)               # regular setup: load and print networks; create schedulers
+                model.parallelize()
+
+            model.set_input(data)  # unpack data from dataset and apply preprocessing
+            model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
+            if len(opt.gpu_ids) > 0:
+                torch.cuda.synchronize()
+            
+            optimize_time = (time.time() - optimize_start_time) / batch_size * 0.005 + 0.995 * optimize_time
+
+            if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
+                save_result = total_iters % opt.update_html_freq == 0
+                model.compute_visuals()
+                save_snapshot_image(model.get_current_visuals())
+                wandb.log({"example": wandb.Image("tmp/current.png")})                
+
+            if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
+                losses = model.get_current_losses()
+                meta = {"epoch": epoch, "epoch_iter": epoch_iter, "time_compute": optimize_time, "time_load": t_data, **losses}
+                wandb.log(meta)
+                print(meta)
+
+            if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
+                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
+                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
+                for save_path in model.save_networks(save_suffix):
+                    wandb.save(save_path)
+
+            iter_data_time = time.time()
+
+        if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
+            print('Saving models at the end of epoch %d, iters %d' % (epoch, total_iters))
+            for save_path in model.save_networks(epoch):
+                    wandb.save(save_path)
+
+        print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
+        model.update_learning_rate()                     # update learning rates at the end of every epoch.
---
+++ util/wutil.py
@@ -0,0 +1,7 @@
+
+import os
+from collections import OrderedDict
+
+from torch import Tensor
+
+from util.util import tensor2im
